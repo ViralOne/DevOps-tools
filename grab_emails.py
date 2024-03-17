72002dbd5e58163a23c9d6d7afd51581a32fb3d8
Mocanu Marian <mocanu.marian@agilefreaks.com>
2024-03-17 02:02:39 +0200

    add email validation before saving

--- grab_emails.py
+++ grab_emails.py
@@ -1,9 +1,13 @@
+# pip install bs4 email-validator
+
 import os
 import re
 import sys
+from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
+from email_validator import validate_email, EmailNotValidError
 
-def extract_emails_from_html(html_file):
+def extract_emails(html_file):
     with open(html_file, 'r', encoding='utf-8') as file:
         soup = BeautifulSoup(file, 'html.parser')
         text_nodes = soup.find_all(string=True)
@@ -14,18 +18,36 @@ def extract_emails_from_html(html_file):
             emails.update(matches)
     return emails
 
+def validator(email, check_deliverability=False):
+    try:
+        if check_deliverability:
+            validate_email(email, check_deliverability=True)
+        else:
+            validate_email(email)
+        return email
+    except EmailNotValidError as e:
+        return e
+
+def validate_emails(emails):
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(lambda email: (email, validator(email, check_deliverability=True)), emails)
+    return results
+
 def main(directory_path):
     all_emails = set()
     for file_name in os.listdir(directory_path):
-        if file_name.endswith('.html'):
-            file_path = os.path.join(directory_path, file_name)
-            emails = extract_emails_from_html(file_path)
-            all_emails.update(emails)
+        file_path = os.path.join(directory_path, file_name)
+        emails = extract_emails(file_path)
+        all_emails.update(emails)
     
-    print("Unique Email Addresses:")
-    for email in all_emails:
-        print(email)
-
+    results = validate_emails(all_emails)
+    for email, result in results:
+        if isinstance(result, EmailNotValidError):
+            print(f"Invalid email: {email}")
+        else:
+            with open('emails.txt', 'a') as f:
+                f.write(email + '\n')
+    
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Grab emails from all html files in a directory")
